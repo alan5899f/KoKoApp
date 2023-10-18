2668fc17c492a1e161a00865a28ba1fff9f42046
alan <alan5899f@gmail.com>
2023-10-18 15:16:43 +0800

    Initial Commit

---
+++ KoKoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KoKoApp
+//
+//  Created by 陳韋綸 on 2023/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
